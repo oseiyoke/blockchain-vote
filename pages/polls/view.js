53222f5679dc3c09daa7e28a10003986f6294304
Ose <osenemedia@gmail.com>
2019-03-22 07:57:23 +0100

    enable voting

--- pages/polls/view.js
+++ pages/polls/view.js
@@ -3,11 +3,13 @@ import Poll from "../../ethereum/poll";
 import web3 from "../../ethereum/web3";
 import { Link } from "../../routes";
 import Layout from "../../components/Layout";
-import { Grid, Message, Card, Button } from "semantic-ui-react";
+import { Grid, Message, Card, Button, Table } from "semantic-ui-react";
+import { Router } from "../../routes";
 
 class PollShow extends Component {
   static async getInitialProps(props) {
     let candidates = [];
+    let candidate_votes = [];
     let numCandidates = 0;
     let title = "";
 
@@ -31,10 +33,20 @@ class PollShow extends Component {
           })
       );
 
+      candidate_votes = await Promise.all(
+        Array(parseInt(numCandidates))
+          .fill()
+          .map((element, index) => {
+            return poll.methods.totalVotes(index).call({
+              from: accounts[0]
+            });
+          })
+      );
+
       title = await poll.methods.title().call();
       title = web3.utils.toAscii(title);
 
-      console.log(candidates);
+      // console.log(candidates);
     } catch (err) {
       console.log(err.message);
     }
@@ -42,6 +54,7 @@ class PollShow extends Component {
     return {
       address: props.query.address,
       candidates,
+      candidate_votes,
       numCandidates,
       title
     };
@@ -51,36 +64,84 @@ class PollShow extends Component {
     super(props);
 
     this.state = {
+      voteButton: [],
       candidates: [],
       errorMessage: "",
-      isHidden: true
+      isHidden: true,
+      loading: false
     };
   }
 
-  renderCards() {
-    const { candidates } = this.props;
-    console.log(candidates);
-
-    let item = [];
-    candidates.forEach(element => {
-      item.push({
-        header: web3.utils.toAscii(element[1]),
-        meta: web3.utils.toAscii(element[2]),
-        description: (
-          <a href={`/polls/${this.props.address}/candidates/${element[0]}`}>
-            View
-          </a>
-        ),
-        style: {
-          overflowWrap: "break-word"
-        }
-      });
+  handleVote = async event => {
+    this.setState({
+      isHidden: true,
+      loading: true,
+      errorMessage: "",
+      voteButton: []
     });
 
-    return <Card.Group items={item} />;
+    let id = event.target.value;
+    console.log(id);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      const poll = Poll(this.props.address);
+
+      await poll.methods.vote(accounts[0], id).send({
+        from: accounts[0]
+      });
+
+      // Router.replace(`/polls/${this.props.address}/`);
+      Router.push(`/polls/${this.props.address}/`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+      console.log(err);
+    }
+
+    this.setState({ loading: false });
+  };
+
+  renderRows = () => {
+    const { Row, Cell } = Table;
+    const { candidates, candidate_votes } = this.props;
+    let item = [];
+
+    candidates.forEach((candidate, index) => {
+      item.push(
+        <Row>
+          <Cell>{candidate[0]}</Cell>
+          <Cell>{web3.utils.toAscii(candidate[1])}</Cell>
+          <Cell>{web3.utils.toAscii(candidate[2])}</Cell>
+          <Cell>{candidate_votes[index]}</Cell>
+        </Row>
+      );
+    });
+
+    return item;
+  };
+
+  async componentDidMount() {
+    let _buttons = [];
+
+    _buttons = this.props.candidates.map((candidate, index) => {
+      return (
+        <Button
+          onClick={this.handleVote}
+          style={{ backgroundColor: "#66cc66" }}
+          value={candidate[0]}
+        >
+          Vote for {web3.utils.toAscii(candidate[1])}
+        </Button>
+      );
+    });
+
+    this.setState({ voteButton: _buttons });
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <Grid style={{ marginTop: "10px" }}>
@@ -96,6 +157,7 @@ class PollShow extends Component {
                     content="Add Candidate"
                     icon="add circle"
                     primary
+                    loading={this.state.loading}
                   />
                 </a>
               </Link>
@@ -118,8 +180,24 @@ class PollShow extends Component {
               </Grid.Row>
 
               <Grid.Row>
-                <Grid.Column>{this.renderCards()}</Grid.Column>
+                <Grid.Column>
+                  <Table>
+                    <Header>
+                      <Row>
+                        <HeaderCell>ID</HeaderCell>
+                        <HeaderCell>Candidate</HeaderCell>
+                        <HeaderCell>Party</HeaderCell>
+                        <HeaderCell>Votes</HeaderCell>
+                      </Row>
+                    </Header>
+                    <Body>{this.renderRows()}</Body>
+                  </Table>
+                </Grid.Column>
               </Grid.Row>
+
+              <br />
+
+              <Grid.Row>{this.state.voteButton}</Grid.Row>
             </Grid.Column>
           </Grid.Row>
         </Grid>
--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@ const routes = require("next-routes")();
 routes
   .add("/polls/new", "/polls/new")
   .add("/polls/:address", "/polls/view")
-  .add("/polls/:address/candidates/:id", "/polls/candidates/view")
   .add("/polls/:address/candidates/new", "/polls/candidates/new")
   .add("/voter/new", "/voter/new");
 
