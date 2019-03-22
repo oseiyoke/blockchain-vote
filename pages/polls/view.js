38dc085db1f1620a751ea084fac8cc1f47177bc4
christopher <okekeobasi@gmail.com>
2019-03-22 11:50:50 +0100

    chore: implement account generator

--- pages/polls/view.js
+++ pages/polls/view.js
@@ -3,7 +3,7 @@ import Poll from "../../ethereum/poll";
 import web3 from "../../ethereum/web3";
 import { Link } from "../../routes";
 import Layout from "../../components/Layout";
-import { Grid, Message, Card, Button, Table } from "semantic-ui-react";
+import { Grid, Message, Card, Button, Table, Input } from "semantic-ui-react";
 import { Router } from "../../routes";
 
 class PollShow extends Component {
@@ -66,6 +66,11 @@ class PollShow extends Component {
     this.state = {
       candidates: [],
       errorMessage: "",
+      voteButton: {
+        display: "none",
+        backgroundColor: "#66cc66",
+        color: "white"
+      },
       isHidden: true,
       loading: false
     };
@@ -129,7 +134,52 @@ class PollShow extends Component {
     return item;
   };
 
-  async componentDidMount() {}
+  renderCards = () => {
+    const { Content, Header, Meta, Description } = Card;
+    const { candidates, candidate_votes } = this.props;
+    let item = [];
+
+    candidates.forEach((candidate, index) => {
+      item.push(
+        <Card>
+          <Content>
+            <Header>
+              {web3.utils.toAscii(candidate[1])}{" "}
+              <span style={{ float: "right" }}>{candidate_votes[index]}</span>
+            </Header>
+            <Meta>{web3.utils.toAscii(candidate[2])}</Meta>
+            <Description>
+              <Button
+                onClick={this.handleVote}
+                style={this.state.voteButton}
+                value={candidate[0]}
+                loading={this.state.loading}
+              >
+                Vote for {web3.utils.toAscii(candidate[1])}
+              </Button>
+            </Description>
+          </Content>
+        </Card>
+      );
+    });
+
+    return item;
+  };
+
+  async componentDidMount() {
+    if (typeof window.web3 === "undefined") {
+      //We are in the browser and metamask is running
+      console.log("No Metamask");
+      this.setState({
+        isHidden: false
+      });
+    } else {
+      this.setState({
+        voteButton: { backgroundColor: "#66cc66", color: "white" },
+        isHidden: true
+      });
+    }
+  }
 
   render() {
     const { Header, Row, HeaderCell, Body } = Table;
@@ -139,20 +189,23 @@ class PollShow extends Component {
         <Grid style={{ marginTop: "10px" }}>
           <Grid.Row>
             <Grid.Column textAlign="center">
-              <h3>{this.props.title}</h3>
-              <h6>{this.props.address}</h6>
-              <br />
-              <Link route={`/polls/${this.props.address}/candidates/new`}>
-                <a>
-                  <Button
-                    floated="right"
-                    content="Add Candidate"
-                    icon="add circle"
-                    primary
-                    loading={this.state.loading}
-                  />
-                </a>
-              </Link>
+              <Grid.Row>
+                <h3>{this.props.title}</h3>
+                <h6>{this.props.address}</h6>
+              </Grid.Row>
+              <Grid.Row>
+                <Link route={`/polls/${this.props.address}/candidates/new`}>
+                  <a>
+                    <Button
+                      floated="right"
+                      content="Add Candidate"
+                      icon="add circle"
+                      primary
+                      loading={this.state.loading}
+                    />
+                  </a>
+                </Link>
+              </Grid.Row>
             </Grid.Column>
           </Grid.Row>
 
@@ -162,18 +215,35 @@ class PollShow extends Component {
                 <Grid.Column>
                   <div style={{ marginBottom: "10px" }}>
                     <Message
-                      error={!!this.state.errorMessage}
-                      hidden={this.state.isHidden}
+                      error={true}
                       header="Oops!"
-                      content={this.state.errorMessage}
-                    />
+                      hidden={this.state.isHidden}
+                    >
+                      <Message.Content>
+                        Your browser doesn't have <b>MetaMask</b>, If you have
+                        an <b>ethereum account</b> input the <b>private key</b>
+                        <br />
+                        <Input
+                          autocomplete={"off"}
+                          placeholder={"Private key"}
+                          style={{ width: "100%" }}
+                        />
+                        <b>else</b> create one
+                        <Link route={`/polls/${this.props.address}/`}>
+                          <a> Here</a>
+                        </Link>
+                      </Message.Content>
+                    </Message>
                   </div>
+                  <br />
                 </Grid.Column>
               </Grid.Row>
 
               <Grid.Row>
                 <Grid.Column>
-                  <Table>
+                  <Card.Group>{this.renderCards()}</Card.Group>
+
+                  {/* <Table>
                     <Header>
                       <Row>
                         <HeaderCell>ID</HeaderCell>
@@ -184,7 +254,7 @@ class PollShow extends Component {
                       </Row>
                     </Header>
                     <Body>{this.renderRows()}</Body>
-                  </Table>
+                  </Table> */}
                 </Grid.Column>
               </Grid.Row>
             </Grid.Column>
